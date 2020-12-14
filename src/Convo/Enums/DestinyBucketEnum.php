cfb60a5df6bb4d5e239dd91dfcb7f5b822892606
Marko Klobucar Ledinscak <mrqoqlobo@gmail.com>
2020-12-14 17:43:43 +0100

    Bucket enum helper class

---
+++ src/Convo/Enums/DestinyBucketEnum.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Convo\Enums;
+
+abstract class DestinyBucketEnum
+{
+    const BUCKET_KINETIC_WEAPONS = 1498876634;
+    const BUCKET_ENERGY_WEAPONS = 2465295065;
+    const BUCKET_POWER_WEAPONS = 953998645;
+
+    const BUCKET_HELMETS = 3448274439;
+    const BUCKET_GAUNTLETS = 3551918588;
+    const BUCKET_CHEST_ARMOR = 14239492;
+    const BUCKET_GREAVES = 20886954;
+    const BUCKET_CLASS_ITEMS = 1585787867;
+}
