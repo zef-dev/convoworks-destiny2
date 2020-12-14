b212141940648768baee629bf65dca9d8d7890d1
Marko Klobucar Ledinscak <mrqoqlobo@gmail.com>
2020-12-14 17:43:50 +0100

    Weapon name catalog

---
+++ src/Convo/Pckg/Destiny/Catalogs/WeaponNameCatalog.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types=1);
+
+namespace Convo\Pckg\Destiny\Catalogs;
+
+use Convo\Api\BaseDestinyApi;
+use Convo\Core\Util\StrUtil;
+use Convo\Enums\DestinyBucketEnum;
+use Convo\Service\DestinyManifestService;
+
+class WeaponNameCatalog implements \Convo\Core\Workflow\ICatalogSource
+{
+    const CATALOG_VERSION = "1";
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $_logger;
+
+    /**
+     * @var \Convo\Core\Util\IHttpFactory
+     */
+    private $_httpFactory;
+
+    private $_manifests;
+
+    public function __construct($logger, $httpFactory)
+    {
+        $this->_logger = $logger;
+        $this->_httpFactory = $httpFactory;
+    }
+
+    public function getCatalogVersion()
+    {
+        return self::CATALOG_VERSION;
+    }
+
+    public function getCatalogValues($platform)
+    {
+        $mservice = new DestinyManifestService($this->_logger, $this->_httpFactory);
+        $this->_manifests = $mservice->initManifest();
+
+        switch ($platform)
+        {
+            case 'amazon':
+                return $this->_getAmazonFormattedNames();
+            case 'dialogflow':
+                return $this->_getDialogflowFormattedNames();
+            default:
+                throw new \Exception("Unexpected platform [$platform]");
+        }
+    }
+
+    private function _getAmazonFormattedNames()
+    {
+        /** @var \SQLite3 $db */
+        $db = $this->_manifests['db'];
+
+        $results = [];
+        $result = $db->query('SELECT * FROM '.BaseDestinyApi::ITEM_TABLE);
+        while($row = $result->fetchArray()) {
+            $key = is_numeric($row[0]) ? sprintf('%u', $row[0] & 0xFFFFFFFF) : $row[0];
+            $results[$key] = json_decode($row[1], true);
+        }
+
+        $weapons = array_filter($results, function($item) {
+            return in_array($item['inventory']['bucketTypeHash'], [
+                DestinyBucketEnum::BUCKET_KINETIC_WEAPONS, DestinyBucketEnum::BUCKET_ENERGY_WEAPONS, DestinyBucketEnum::BUCKET_POWER_WEAPONS
+            ]);
+        });
+
+        $formatted = [
+            'values' => array_map(function ($weapon) {
+                return [
+                    'id' => StrUtil::slugify($weapon['displayProperties']['name']),
+                    'name' => [
+                        'value' => $weapon['displayProperties']['name']
+                    ]
+                ];
+            }, $weapons)
+        ];
+
+        return $formatted;
+    }
+
+    private function _getDialogflowFormattedNames()
+    {
+        /** @var \SQLite3 $db */
+        $db = $this->_manifests['db'];
+
+        $results = [];
+        $result = $db->query('SELECT * FROM '.BaseDestinyApi::ITEM_TABLE);
+        while($row = $result->fetchArray()) {
+            $key = is_numeric($row[0]) ? sprintf('%u', $row[0] & 0xFFFFFFFF) : $row[0];
+            $results[$key] = json_decode($row[1], true);
+        }
+
+        $weapons = array_filter($results, function($item) {
+            return in_array($item['inventory']['bucketTypeHash'], [
+                DestinyBucketEnum::BUCKET_KINETIC_WEAPONS, DestinyBucketEnum::BUCKET_ENERGY_WEAPONS, DestinyBucketEnum::BUCKET_POWER_WEAPONS
+            ]);
+        });
+
+        return array_map(function ($weapon) { return $weapon['displayProperties']['name']; }, $weapons);
+    }
+}
